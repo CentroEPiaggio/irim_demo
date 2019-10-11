7f25585e45bd7b7b32c87cd5a547f045ab2cca63
George Jose Pollayil <gpollayil@gmail.com>
2019-10-11 17:00:46 +0200

    Finished coding the identifier; it seems to work correctly execpt for some problems in color identification

--- irim_vision/src/clusters_identifier.cpp
+++ irim_vision/src/clusters_identifier.cpp
@@ -20,31 +20,36 @@ Email: gpollayil@gmail.com, mathewjosepollayil@gmail.com  */
 // Defines
 #define     DEBUG   1       // Prints out additional stuff
 
+/* 
+    BEWARE: the ws rectangle, colors and the basic pose orientation are hard coded
+    TODO: Parse these values! 
+*/
 
 // Defining colors struct and some basic colors
 struct Color {
 
     // Constructora
     Color() {
-        this->name = "black";
-        this->r = 0; this->g = 0; this->b = 0;
+        this->name = "red";
+        this->r = 255; this->g = 0; this->b = 0; this->id = 1;
     }
 
-    Color(std::string name_, int r_, int g_, int b_) {
+    Color(std::string name_, int r_, int g_, int b_, int id_) {
         this->name = name_;
-        this->r = r_; this->g = g_; this->b = b_;
+        this->r = r_; this->g = g_; this->b = b_; this->id = id_;
     }
 
     // Variables
     std::string name;
     int r, g, b;
+    int id;
 };
 
-Color red("red", 255, 0, 0);            // obj_id = 1
-Color green("green", 0, 255, 0);        // obj_id = 2
-Color blue("blue", 0, 0, 255);          // obj_id = 3
-Color black("black", 0, 0, 0);          // obj_id = 4
-Color white("red", 255, 255, 255);      // obj_id = 5
+Color red("red", 255, 0, 0, 1);            // obj_id = 1
+Color green("green", 0, 255, 0, 2);        // obj_id = 2
+Color blue("blue", 0, 0, 255, 3);          // obj_id = 3
+Color black("black", 0, 0, 0, 4);          // obj_id = 4
+Color white("red", 255, 255, 255, 5);      // obj_id = 5
 
 
 // Class definition
@@ -61,6 +66,10 @@ class ClustersIdentifier {
 
         // Build the colors and pushback
         this->known_colors = {red, green, blue, black, white};
+
+        // Assign the basic orientation
+        this->basic_quat.x = 0.0; this->basic_quat.y = 0.0;
+        this->basic_quat.z = 0.0; this->basic_quat.w = 1.0;
 
     }
 
@@ -80,17 +89,43 @@ class ClustersIdentifier {
     std::vector<Color> known_colors;
 
     // PCL Elements
-    pcl::PCLPointCloud2 tmp_cluster;
-    pcl::PointCloud<pcl::PointXYZRGB> cluster_xyzrgb;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster_xyzrgb_ptr;
 
     // Other vars
     irim_vision::SegmentedClustersArray saved_msg;
+
+    // RECTAGLE OF WORKSPACE (only clusters inside this rectangle are considered)
+    // TODO: Parse these values
+    const double x_lo = 0.20; const double x_up = 0.70;
+    const double y_l = 0.10; const double y_r = -0.40;
+
+    // Basic orientation
+    geometry_msgs::Quaternion basic_quat;
 
     // Callback for the segmented clusters array
     void cluster_cb(const irim_vision::SegmentedClustersArrayConstPtr& cloud_msg);
 
     // Function for assigning id according to color and poses through centroid computation
-    int assign_pose_id(sensor_msgs::PointCloud2 cluster_in, geometry_msgs::Pose& assigned_pose);
+    bool assign_pose_id(sensor_msgs::PointCloud2 cluster_in, geometry_msgs::Pose& assigned_pose, int& assigned_id);
+
+    // Aux function to unpack rgb of PointXYZRGB
+    inline void unpack_rgb(pcl::PointXYZRGB point_in, int& r_out, int& g_out, int& b_out) {
+        uint32_t rgb = *reinterpret_cast<int*>(&point_in.rgb);
+        uint8_t r = (rgb >> 16) & 0x0000ff;
+        uint8_t g = (rgb >> 8)  & 0x0000ff;
+        uint8_t b = (rgb)       & 0x0000ff;
+        r_out = r; g_out = g; b_out = b;
+    }
+
+    // Aux function for checking if point inside workspace rectangle
+    inline bool is_inside(pcl::PointXYZRGB point_in) {
+        if (point_in.x > x_lo && point_in.x < x_up) {
+            if (point_in.y > y_r && point_in.y < y_l) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }; // End class definition
 
@@ -106,17 +141,23 @@ void ClustersIdentifier::cluster_cb(const irim_vision::SegmentedClustersArrayCon
     for (int i = 0; i < this->saved_msg.clusters.size(); i++) {
 
         geometry_msgs::Pose tmp_pose;
+        int tmp_id;
 
-        // Assigning the id
-        tmp_ident_cluster.obj_id = assign_pose_id(this->saved_msg.clusters.at(i), tmp_pose);
+        // Assigning the id and pose and checking if inside ws (if not go to next cluster)
+        bool inside = assign_pose_id(this->saved_msg.clusters.at(i), tmp_pose, tmp_id);
+        if (!inside) {
+            ROS_WARN("The processed cluster is outside the ws!");
+            continue;
+        }
 
-        // Assigning pose
+        // Assigning pose and id
         tmp_ident_cluster.pose = tmp_pose;
+        tmp_ident_cluster.obj_id = tmp_id;
 
         // Assigning header
         tmp_header.stamp = ros::Time::now();
         tmp_ident_cluster.header = tmp_header;
-
+        
         // Pushing back
         ouput_msg.ident_clusters.push_back(tmp_ident_cluster);
 
@@ -128,34 +169,62 @@ void ClustersIdentifier::cluster_cb(const irim_vision::SegmentedClustersArrayCon
 }
 
 // Function for assigning id according to color
-int ClustersIdentifier::assign_pose_id(sensor_msgs::PointCloud2 cluster_in, geometry_msgs::Pose& assigned_pose) {
+bool ClustersIdentifier::assign_pose_id(sensor_msgs::PointCloud2 cluster_in, geometry_msgs::Pose& assigned_pose, int& assigned_id) {
 
     int closest_id = 1;
     double closest_dist = 3 * std::pow(255, 2);
 
     // Converting to PCL format
-    pcl_conversions::toPCL(cluster_in, this->tmp_cluster);
-    pcl::fromPCLPointCloud2(this->tmp_cluster, this->cluster_xyzrgb);
+    this->cluster_xyzrgb_ptr.reset(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::fromROSMsg(cluster_in, *this->cluster_xyzrgb_ptr);
 
     // Computing the average color of the cluster
     pcl::CentroidPoint<pcl::PointXYZRGB> centroid_finder;
     pcl::PointXYZRGB curr_centroid;
-    for (auto it : cluster_xyzrgb.points) {
+    for (auto it : cluster_xyzrgb_ptr->points) {
         centroid_finder.add(it);
-        if (DEBUG) ROS_INFO_STREAM("The current point of the cluster has col: (" << it.r << ", " << it.g << ", " << it.b << ")");
     }
     centroid_finder.get(curr_centroid);
 
-    if (DEBUG) ROS_INFO_STREAM("The centroid of the cluster is pos: (" << curr_centroid.x << ", " << curr_centroid.y << ", " << 
-        curr_centroid.z << ") / col: (" << curr_centroid.r << ", " << curr_centroid.g << ", " << curr_centroid.b << ")");
+    // Checking if centroid inside ws
+    if (!this->is_inside(curr_centroid)) return false;
 
-    // // Iterating the colors and checking the nearest
-    // for (int i = 0; i < this->known_colors.size(); i++) {
+    // Unpacking and couting
+    int r, g, b; 
+    if (DEBUG) {
+        this->unpack_rgb(curr_centroid, r, g, b);
+        ROS_INFO_STREAM("The centroid of the cluster is pos: (" << curr_centroid.x << ", " << curr_centroid.y << ", " << 
+            curr_centroid.z << ") / col: (" << r << ", " << g << ", " << b << ")");
+    }
 
-    //     int curr_dist = 
-    //     if (this->)
+    // Iterating the colors and checking the nearest and choosing id accordingly
+    int min_dist = 3 * std::pow(255, 2);
+    int chosen_id = 0;
+    for (auto it : this->known_colors) {
 
-    // }
+        int this_distance = std::pow(it.r-r,2) + std::pow(it.g-g,2) + std::pow(it.b-b,2);
+        if (this_distance < min_dist) {
+            min_dist = this_distance;
+            chosen_id = it.id;
+        }
+
+    }
+
+    if (DEBUG) {
+        ROS_INFO_STREAM("The id (color) of the cluster is " << chosen_id << "!");
+    }
+
+    // Assigning the pose of the cluster
+    geometry_msgs::Pose ass_pose;                   // Ass! Hihihihi XD
+    ass_pose.position.x = curr_centroid.x;
+    ass_pose.position.y = curr_centroid.y;
+    ass_pose.position.z = curr_centroid.z;
+    ass_pose.orientation = this->basic_quat;
+
+    // Passing through ref and returning
+    assigned_id = chosen_id;
+    assigned_pose = ass_pose;
+    return true;
 
 }
 
