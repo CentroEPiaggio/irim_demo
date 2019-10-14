36dca73910c627337d3e9ffddb5cae9d76ccd0ea
George Jose Pollayil <gpollayil@gmail.com>
2019-10-14 12:16:26 +0200

    Removed the distraction error of still using realsense link to rgb transform

--- irim_utils/scripts/object_camera_publisher_pcl.py
+++ irim_utils/scripts/object_camera_publisher_pcl.py
@@ -44,8 +44,8 @@ robot_id = 0
 # rob_marker = [0.17, 0, -0.025, -1.5707, -3.1415, 0]
 # rob_marker = [0, 0, 0, 0, 0, 0]
 rob_marker = [0.02, -0.285, 0.0, 1.5707, 0, 3.1415]
-cam_rgb_2_link = [0.015, -0.000, -0.000, -0.499, 0.502, -0.500, -0.499]        # For Intel Realsense
-# cam_rgb_2_link = [-0.045, 0.000, 0.000, 0.500, -0.500, 0.500, 0.500]      # For Asus Xtion
+# cam_rgb_2_link = [0.015, -0.000, -0.000, -0.499, 0.502, -0.500, -0.499]        # For Intel Realsense
+cam_rgb_2_link = [-0.045, 0.000, 0.000, 0.500, -0.500, 0.500, 0.500]      # For Asus Xtion
 
 world_frame_name = "world"
 object_frame_name = "object"
